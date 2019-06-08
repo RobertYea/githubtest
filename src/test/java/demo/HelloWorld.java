ecb4e4d53d5eea248d16a629de57d51f1f24ca5e
Ye <robertyea@163.com>
2019-06-08 14:45:17 +0800

    Initial commit

---
+++ src/test/java/demo/HelloWorld.java
@@ -0,0 +1,12 @@
+package demo;
+
+/**
+ * @Author:Ye
+ * @Date:2019/6/8
+ * @Time:14:42
+ **/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
